ad93aa6af4ea9310b398a54b3041a7dc47b3fe5e
Jane <jane.e.nath@gmail.com>
2020-02-29 00:55:11 -0800

    reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -118,4 +118,4 @@ image: "https://images.unsplash.com/photo-1505923080474-19e930fb03e7?ixlib=rb-1.
 ])
 
 users = User.create({name: "test user", username: "test",
-password_digest: "$2y$12$t8MvkYpRzSoNl/PYyeG7E.ocGQe.prfKIDWnyDB1dHsLzZQf8f5ti"})
+password_digest: "test"})
